dad19a55727937568d6934349ed1981b7345d235
admin <admin@Bcalab4-m19>
2022-03-09 10:44:46 +0530

    okokokok

--- GithubLab2/src/Student.java
+++ GithubLab2/src/Student.java
@@ -1,8 +1,16 @@
 
 public class Student {
+	String Name;
+	
+	Student(String name) 
+	
+	{
+		this.Name = name;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
